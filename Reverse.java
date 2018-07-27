1829640dfff42d13d6c0dc499b5bfe57b34c08de
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-27 17:08:54 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+package janani;
+import java.util.Scanner;
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,a;
+		System.out.println("enter the no");
+		Scanner ja=new Scanner(System.in);
+		n=ja.nextInt();
+	
+		while(n>0){
+			a=n%10;
+			n=n/10;
+			System.out.print(a);
+		}
+
+	}
+
+}
